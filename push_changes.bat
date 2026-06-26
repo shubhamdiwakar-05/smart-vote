4479d9aa5673bf5adf640bfe547520343e3214cc
SmartVote Dev <dev@smartvote.com>
2026-06-27 00:24:32 +0530

    results

--- push_changes.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set "PATH=%PATH%;C:\Program Files\Git\cmd"
-"C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "Add beautiful mock data to ResultsPage"
-"C:\Program Files\Git\cmd\git.exe" push origin master
